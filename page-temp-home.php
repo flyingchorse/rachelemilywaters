64b195202798b7af91f4d6d54456a57d833a4660
David Griffin <dave@digidol-media.net>
2021-05-13 13:38:48 +0100

    Remove trailing >

--- page-temp-home.php
+++ page-temp-home.php
@@ -10,14 +10,15 @@
 
 get_header(); ?>
 		<div class="feature-section">
-		<?php $sticky = 'Fundraising';
+		<?php $sticky = get_option( 'sticky_posts' );
 $args = array(
 	'posts_per_page' => 3,
 	'post__in'  => $sticky,
 	'ignore_sticky_posts' => 1
 );
-//query_posts( $args );
-
+query_posts( $args );
+if ( $sticky[0] ) {
+	$featurecount = 1;
 	while (have_posts()) : the_post();
 	// insert here your stuff...
 	?>
@@ -32,7 +33,7 @@ $args = array(
 			<?php
 			$featurecount++;
 		endwhile;		
-?>			
+};?>			
 		<div class="clear-both"></div>	
 			
 		</div>
@@ -42,8 +43,8 @@ $args = array(
 	<?php		 
 	$options = rachelemilywaters_get_theme_options();
 
-//$args = array('cat' => $options['sample_select_options'], 'posts_per_page' => 4, 'ignore_sticky_posts' => 1 );
-  //  query_posts($args);
+$args = array('cat' => $options['sample_select_options'], 'posts_per_page' => 4, 'ignore_sticky_posts' => 1 );
+    query_posts($args);
 if (have_posts()) : 
 ?>
 <div class="recent-post-section">
